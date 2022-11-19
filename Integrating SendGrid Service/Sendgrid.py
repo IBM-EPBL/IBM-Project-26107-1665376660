c6d5ab5391b826aac88a532b12abb5ad3b9b3fd0
PONBASKAR.U <84491246+iambaskar@users.noreply.github.com>
2022-11-19 11:21:11 +0530

    Update Sendgrid.py

--- Integrating SendGrid Service/Sendgrid.py
+++ Integrating SendGrid Service/Sendgrid.py
@@ -1,18 +1,37 @@
 # using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-message = Mail(
-    from_email='from_email@example.com',
-    to_emails='to@example.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
-try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.message)
+# from_address we pass to our Mail object, edit with your name
+FROM_EMAIL = 'Your_Name@SendGridTest.com'
+
+
+def SendEmail(to_email):
+    """ Send an email to the provided email addresses
+
+    :param to_email = email to be sent to
+    :returns API response code
+    :raises Exception e: raises an exception """
+    message = Mail(
+        from_email=FROM_EMAIL,
+        to_emails=to_email,
+        subject='A Test from SendGrid!',
+        html_content='<strong>Hello there from SendGrid your URL is: ' +
+        '<a href=''https://github.com/cyberjive''>right here!</a></strong>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        code, body, headers = response.status_code, response.body, response.headers
+        print(f"Response Code: {code} ")
+        print(f"Response Body: {body} ")
+        print(f"Response Headers: {headers} ")
+        print("Message Sent!")
+    except Exception as e:
+        print("Error: {0}".format(e))
+    return str(response.status_code)
+
+
+if __name__ == "__main__":
+    SendEmail(to_email=input("Email address to send to? "))
